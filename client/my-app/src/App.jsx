9ee53b19be78c04c57ab1b48ff4eed6bbd9939cf
Keely Bailey <keelybailey3@gmail.com>
2022-01-15 23:13:45 -0600

    Set up component folders

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -1,9 +1,9 @@
-import NavBar from "./navbar/NavBar.jsx";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      Blog Page
     </div>
   );
 }
