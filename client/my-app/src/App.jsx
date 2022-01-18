e856cbed5b6aa6ba575dcd14f57e66a9ce9d5036
Keely Bailey <keelybailey3@gmail.com>
2022-01-17 21:54:53 -0600

    Blog Post component

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import BlogPost from "./components/BlogPost";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <BlogPost />
     </div>
   );
 }
---
+++ client/my-app/src/components/BlogPost/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function BlogPost() {
+  return (
+    <div className="container mt-3">
+    <div className="row align-items-center">
+      <div className="col-md-6 offset-md-3">
+        <h6>CATEGORY</h6>
+        <h1>Blog Title</h1>
+        <h6>Date</h6>
+        <p>
+        Id cubilia ante ornare volutpat vitae urna quam ut elit nascetur eu. Augue est elementum dictum semper finibus nisi volutpat. Fusce sociosqu odio euismod suspendisse quisque vitae nascetur mauris hendrerit senectus vestibulum. Eu congue turpis nulla nullam potenti enim urna curae euismod duis. Sagittis hendrerit pharetra volutpat pellentesque bibendum erat lectus diam vulputate parturient. Imperdiet tellus hac nulla laoreet nascetur mus iaculis. Natoque fringilla nullam aliquet vivamus justo. Odio vulputate ante elementum mi urna feugiat egestas tellus.
+        </p>
+
+        <p>
+        Convallis massa dui arcu aenean ligula morbi non dapibus. Enim dui mauris sociosqu libero fusce magna nisi augue est hac. In potenti dis conubia cubilia sapien. Hendrerit mus auctor mauris ligula urna pellentesque senectus. Litora risus augue placerat neque non at magnis aliquam consectetuer faucibus. Malesuada vel egestas mollis lectus felis enim himenaeos senectus. Suspendisse dis magnis fermentum natoque luctus dolor malesuada hac adipiscing nibh.
+        </p>
+    </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default BlogPost;
