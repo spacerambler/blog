cee4b2988fd0cf3e10795385149c113e9d3e1469
Keely Bailey <keelybailey3@gmail.com>
2022-01-18 20:39:17 -0600

    Post Seed

--- server/app/db/seed.js
+++ server/app/db/seed.js
@@ -1,10 +1,18 @@
 // TODO: Import the models
+import Post from "../models/Post.js";
 import client from "./client.js";
 
 client
   .connect()
   .then(() => {
-    // TODO: Seed your database here!
+    Promise.all([
+      Post.create({
+        title: "Blog Title Test",
+        content: "Woooooooow",
+        tags: ["Test"],
+        date: "01/18/2022",
+      }),
+    ]);
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err.message);
