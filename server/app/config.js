1a90fcbaf1e5c5a5cfdf054a0074b506f5ee8587
Keely Bailey <keelybailey3@gmail.com>
2022-01-20 19:51:35 -0600

    Restart server file

--- server/app/config.js
+++ server/app/config.js
@@ -3,13 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  // TODO: Add additional configs 🔧 here.
-  db: {
-    url: process.env.DB_CLIENT_URL || "mongodb://localhost/sr-blog",
-  },
-  jwt: {
-    expirationTime: process.env.JWT_EXPIRATION || "1d",
-    secret: process.env.JWT_SECRET || "secrets",
-  },
-  port: process.env.PORT || 4000,
+  port: process.env.PORT || 3000,
+  db: process.env.DB_CLIENT_URL,
 };
