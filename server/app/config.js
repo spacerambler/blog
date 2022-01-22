761d192741f7e7d8a6ada7bfb858089b50b4dbd6
Keely Bailey <keelybailey3@gmail.com>
2022-01-21 21:35:50 -0600

    Connect to mongodb

--- server/app/config.js
+++ server/app/config.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
+  db: {
+    url: process.env.DB_CLIENT_URL,
+  },
   port: process.env.PORT || 3000,
-  db: process.env.DB_CLIENT_URL,
 };
--- server/app/index.js
+++ server/app/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import config from "./config.js";
+
+const { port } = config;
+const { db } = config;
 
 const app = express();
-const PORT = process.env.PORT || 4444;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.use(
   express.json(),
@@ -9,4 +22,4 @@ app.use(
   express.static("public")
 );
 
-app.listen(PORT, () => console.log("server running"));
+app.listen(port, () => console.log("server running"));
